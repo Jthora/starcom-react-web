e9e8155cc902f5303d60f8f48a75220fee703a0f
Jordan Trana <jordan.trana@gmail.com>
2023-08-25 10:44:00 -0700

    clean up fixing texture

--- starcom/src/App.js
+++ starcom/src/App.js
@@ -1,25 +1,23 @@
 import './App.css';
 import Globe from 'react-globe.gl';
 import React from 'react'
-import { useEffect, useState, useMemo, d3 } from "react";
+import { useEffect, useState } from "react";
 
 import globeImageUrl from "./assets/earthmap1k.jpeg";
-import bumpImageUrl from "./assets/earthbump1k.jpeg";
-// import globeImageUrl from "./assets/earthmap1k.jpeg";
-// import globeImageUrl from "./assets/earthmap1k.jpeg";
-// import globeImageUrl from "./assets/earthmap1k.jpeg";
+//import bumpImageUrl from "./assets/earthbump1k.jpeg";
 
 function World () {
-  const [countries, setCountries] = useState({ features: []});
+  const [countries, setCountries] = useState({ features: [] });
 
   useEffect(() => {
     // load data
-    fetch("./data/ne_110m_admin_0_countries.geojson").then(res => res.json()).then(setCountries);
+    fetch("./data/ne_110m_admin_0_countries.geojson") // Correct the path here
+      .then(res => res.json())
+      .then(setCountries);
   }, []);
 
   return <Globe
-    //globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
-    globeImageUrl="//unpkg.com/three-globe/example/img/earth-dark.jpg"
+    globeImageUrl={globeImageUrl}
     backgroundImageUrl="//unpkg.com/three-globe/example/img/night-sky.png"
 
     hexPolygonsData={countries.features}
@@ -34,7 +32,6 @@ function World () {
 };
 
 function App() {
-  const myData = [];
   return (
     <div className="App">
       <World />
